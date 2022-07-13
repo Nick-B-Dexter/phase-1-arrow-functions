39a22022b71baa6e4fca335675d658e587ab5ec0
Nick Dexter <nickdexter23@gmail.com>
2022-07-13 00:07:53 -0500

    commit tree e95a0732597dc41a9970a9843cdcc5f1f098593d

---
+++ index.js
@@ -0,0 +1,8 @@
+const divide = (parameter1, parameter2) => parameter1/parameter2;
+divide(2000,100);
+
+const square = parameter => parameter*parameter;
+square();
+
+const add = (parameter3, parameter4) => parameter3 + parameter4;
+add();
